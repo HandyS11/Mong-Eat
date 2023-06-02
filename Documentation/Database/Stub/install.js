9764190b5e63ca2f71336906cc5649c54de23cb4
Valentin Clergue <valentin.clergue@etu.uca.fr>
2023-06-02 09:55:16 +0200

    not working

--- Documentation/Database/Stub/install.js
+++ Documentation/Database/Stub/install.js
@@ -1,16 +1,16 @@
-const { ArticlesStub } = require('./Articles')
-const { UsersStub } = require('./Users')
-const { RestaurantsStub } = require('./Restaurants')
-const { OrdersStub } = require('./Orders')
+import UsersStub from "./Users.js"
+import ArticlesStub from "./Articles.js"
+import RestaurantsStub from "./Restaurants.js"
+import OrdersStub from "./Orders.js"
 
 db = connect('mongodb://localhost:27017/MongEat')
 db.dropDatabase()
 
-print('Inserting articles...')
-db.Articles.insertMany(ArticlesStub)
-
 print('Inserting users...')
 db.Users.insertMany(UsersStub)
+
+print('Inserting articles...')
+db.Articles.insertMany(ArticlesStub)
 
 print('Inserting restaurants...')
 db.Restaurant.insertMany(RestaurantsStub)

40a2706925df18ffae8959e67fab02e4a099a422
HandyS11 <valentin-clergue@orange.fr>
2023-06-24 00:10:47 +0200

    dafuck

--- Sources/src/main/java/com/mongeat/dtos/UserDto.java
+++ Sources/src/main/java/com/mongeat/dtos/UserDto.java
@@ -1,6 +1,7 @@
 package com.mongeat.dtos;
 
 import com.mongeat.dtos.parts.LocationDto;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@RegisterForReflection
 public class UserDto extends GenericDto {
     private String firstName;
     private String lastName;
